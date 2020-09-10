b68ab9b68e15a5d2ef58db6113419ddbe6e4ebc9
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-09-10 10:28:51 -0700

    C# tree traversal initial commit

---
+++ tree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tree
+{
+    class Node
+    {
+        public string Id { get; set; }
+
+        public List<Node> Children { get; set; }
+
+        public Node(String id)
+        {
+            this.Id = id;
+            this.Children = new List<Node>();
+        }
+    }
+}
---
+++ tree/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using tree;
+
+namespace tree
+{
+    class Program
+    {
+        static Node manager;
+        static List<Node> employees = new List<Node>();
+
+
+        static void Main(string[] args)
+        {
+            Node root = GenerateTree();
+            //Finding node 'B'
+            Find("B", root);
+            //Getting the childrens of node 'B'
+            Collect(manager);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Id);
+            }
+        }
+        /***
+         * Tree structure
+         * 
+         * A
+         * |
+         * +--B
+         *    |
+         *    +--C
+         *    +--D
+         *    +--E
+         *    F
+         *    |
+         *    +--G
+         *    +--H
+         *    +--I
+         *    +--J
+         *       |
+         *       +--Z
+         ***/
+        public static Node GenerateTree()
+        {
+            //right
+            Node G = new Node("G");
+            Node H = new Node("H");
+            Node Z = new Node("Z");
+            Node J = new Node("J");
+            J.Children.Add(Z);
+            Node I = new Node("I");
+            Node F = new Node("F");
+            F.Children.Add(G);
+            F.Children.Add(H);
+            F.Children.Add(I);
+            F.Children.Add(J);
+            //Left
+            Node C = new Node("C");
+            Node D = new Node("D");
+            Node E = new Node("E");
+            Node B = new Node("B");
+            B.Children.Add(C);
+            B.Children.Add(D);
+            B.Children.Add(E);
+            //root
+            Node A = new Node("A");
+            A.Children.Add(B);
+            A.Children.Add(F);
+            //return rootnode
+            return A;
+        }
+        
+        static void Find(String id, Node node)
+        {
+            foreach (var child in node.Children)
+            {
+                Find(id, child);
+                if (child.Id == id)
+                {
+                    manager = child;
+                }
+              
+            }
+        }
+
+        static void Collect(Node node)
+        {
+            foreach (var child in node.Children)
+            {
+                Collect(child);
+                employees.Add(child);
+            }
+        }
+
+         
+    }
+}
+
